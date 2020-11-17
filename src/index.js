060e35e69d8486b29059a1d174221622ff00d807
Harvey Granfil <jhgranfil@gmail.com>
2020-11-17 08:37:27 +0800

    fixed favicon

--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ import { homePage } from "./pages/home.js";
 import { aboutPage } from "./pages/about.js";
 import { menuPage } from "./pages/menu.js";
 import { contactPage } from "./pages/contact.js";
+import favicon from "/assets/burger-favicon.png";
 import "./main.css";
+
+(function () {
+  var link =
+    document.querySelector("link[rel*='icon']") ||
+    document.createElement("link");
+  link.type = "image/x-icon";
+  link.rel = "shortcut icon";
+  link.href = favicon;
+  document.getElementsByTagName("head")[0].appendChild(link);
+})();
 
 const content = document.querySelector(".content");
 content.append(initPage());
@@ -18,40 +29,38 @@ mainContent.append(contactPage());
 const nav = new Navigation();
 const pages = document.querySelectorAll(".pages");
 for (let i = 1; i < pages.length; i++) {
-  pages[i].classList.add('hide')
+  pages[i].classList.add("hide");
 }
 
 const hidePages = () => {
   for (let i = 0; i < pages.length; i++) {
-    pages[i].classList.add('hide')
+    pages[i].classList.add("hide");
   }
-}
+};
 
-const ctaBtn = document.querySelector('.cta-btn');
-ctaBtn.addEventListener('click', function(){
+const ctaBtn = document.querySelector(".cta-btn");
+ctaBtn.addEventListener("click", function () {
   const menu = document.querySelector(".menu-page");
-  hidePages()
-  menu.classList.remove('hide')
-})
-
+  hidePages();
+  menu.classList.remove("hide");
+});
 
 nav.homeClick(() => {
   const home = document.querySelector(".home-page");
-  hidePages()
-  home.classList.remove('hide')
+  hidePages();
+  home.classList.remove("hide");
 });
 
 nav.aboutClick(() => {
   const about = document.querySelector(".about-page");
-  hidePages()
-  about.classList.remove('hide')
+  hidePages();
+  about.classList.remove("hide");
 });
 
 nav.menuClick(() => {
   const menu = document.querySelector(".menu-page");
-  hidePages()
-  menu.classList.remove('hide')
-
+  hidePages();
+  menu.classList.remove("hide");
 });
 
 nav.contactClick(() => {
